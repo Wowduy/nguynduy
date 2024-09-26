f5cd1f4c1a1e58afb4b54a3c142c04c0118b012b
Wowduy <161957830+Wowduy@users.noreply.github.com>
2024-09-26 15:58:43 +0700

    Add files via upload
    BÀI KIỂM TRA  26/9

---
+++ baikiemtra/bai1/BAIKIEMTRA269/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+namespace InheritanceExample
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Age: {Age}");
+        }
+    }
+    public class Student : Person
+    {
+        public string StudentID { get; set; }
+
+      
+        public override void ShowInfo()
+        {
+            base.ShowInfo(); 
+            Console.WriteLine($"Student ID: {StudentID}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student
+            {
+                Name = "nguyen duy",
+                Age = 20,
+                StudentID = "22810310021" 
+            };
+
+            student.ShowInfo();
+        }
+    }
+}
---
+++ baikiemtra/bai2/bai2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+interface IShape
+{
+    double CalculateArea();
+}
+
+class Circle : IShape
+{
+    public double Radius { get; set; }
+
+    public double CalculateArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+}
+
+class Rectangle : IShape
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public double CalculateArea()
+    {
+        return Width * Height;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Circle circle = new Circle();
+        Rectangle rectangle = new Rectangle();
+
+        Console.WriteLine("nhap ban kinh hinh tron:");
+        circle.Radius = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("nhap chieu rong hinh chu nhat:");
+        rectangle.Width = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("nhap chieu dai hinh chu nhat:");
+        rectangle.Height = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine($"dien tich hinh tron: {circle.CalculateArea():F2}");
+        Console.WriteLine($"dien tich hinh chu nhat: {rectangle.CalculateArea():F2}");
+    }
+}
